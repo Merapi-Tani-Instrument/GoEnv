55da03c17690ae9439d9c8d5c4a45e8eb8edeeea
Fahrul putra pratama <33424526+fahrulputra40@users.noreply.github.com>
2024-01-16 05:22:27 +0700

    Add files via upload

--- goenv.go
+++ goenv.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io/fs"
+	"os"
 	"regexp"
 	"strings"
 )
@@ -11,6 +12,8 @@ import (
 const (
 	TAB_SIZE = 2
 )
+
+type GoEnv map[string]string
 
 func pop(alist *[]string) {
 	f := len(*alist)
@@ -56,10 +59,11 @@ func Parse(fs fs.File) (map[string]string, error) {
 		if ll := regexp.MustCompile(fmt.Sprintf("(%s)", tabReg)).FindAllString(strings.Split(line, ":")[0], -1); ll != nil {
 			index = len(ll)
 		}
-		resultProps := regexp.MustCompile(`(.+)(?:\:)`).FindStringSubmatch(line)
+		resultProps := regexp.MustCompile(`(.+)(?:.)`).FindStringSubmatch(line)
 		if len(resultProps) < 2 {
 			continue
 		}
+
 		if index == 0 {
 			prop = nil
 			prop = append(prop, resultProps[1])
@@ -91,6 +95,14 @@ func Parse(fs fs.File) (map[string]string, error) {
 			res[fmt.Sprintf("%s%s", strings.Join(prop, "."), idx)] = strings.TrimSpace(value[1])
 		}
 	}
-	fmt.Println(res)
+	fmt.Println("res ", res)
 	return res, nil
+}
+
+func (g GoEnv) SetAsEnvironment() {
+	for k, v := range g {
+		if _, ok := os.LookupEnv(k); !ok {
+			os.Setenv(k, v)
+		}
+	}
 }
