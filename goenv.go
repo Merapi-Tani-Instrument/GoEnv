7fb58599b3cb05b7129ebcc467991779ae8ff5be
Fahrul putra pratama <33424526+fahrulputra40@users.noreply.github.com>
2024-01-16 11:44:24 +0700

    Add files via upload

--- goenv.go
+++ goenv.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
+	"reflect"
 	"regexp"
 	"strings"
 )
@@ -48,25 +49,23 @@ func Parse(fs fs.File) (GoEnv, error) {
 			if output.Len() > 0 {
 				output.WriteString("\n")
 			}
-			fmt.Println("ignore: ", ignore)
 			continue
 		}
 		tabReg := ""
 		for i := 0; i < TAB_SIZE; i++ {
-			tabReg += "\\s"
+			tabReg += `\s`
 		}
 		index := 0
 		if ll := regexp.MustCompile(fmt.Sprintf("(%s)", tabReg)).FindAllString(strings.Split(line, ":")[0], -1); ll != nil {
 			index = len(ll)
 		}
-		resultProps := regexp.MustCompile(`(.+)(?:.)`).FindStringSubmatch(line)
+		resultProps := regexp.MustCompile(`(.+):[\s.]?`).FindStringSubmatch(line)
 		if len(resultProps) < 2 {
 			continue
 		}
-
 		if index == 0 {
 			prop = nil
-			prop = append(prop, resultProps[1])
+			prop = append(prop, strings.TrimSpace(resultProps[1]))
 		} else {
 			propName := ""
 			if arrayLine != "" {
@@ -92,17 +91,52 @@ func Parse(fs fs.File) (GoEnv, error) {
 			if arrayIndex >= 0 {
 				idx = fmt.Sprintf("[%d]", arrayIndex)
 			}
-			res[fmt.Sprintf("%s%s", strings.Join(prop, "."), idx)] = strings.TrimSpace(value[1])
+			key := fmt.Sprintf("%s%s", strings.Join(prop, "."), idx)
+			if v, ok := os.LookupEnv(key); ok {
+				res[key] = v
+			} else {
+				res[key] = strings.TrimSpace(value[1])
+			}
 		}
 	}
-	fmt.Println("res ", res)
-	return res, nil
+	return GoEnv(res), nil
 }
 
 func (g GoEnv) SetAsEnvironment() {
 	for k, v := range g {
 		if _, ok := os.LookupEnv(k); !ok {
 			os.Setenv(k, v)
+		}
+	}
+}
+
+func (g GoEnv) Inject(param ...any) {
+	for _, p := range param {
+		t := reflect.TypeOf(p)
+		if t.Kind() != reflect.Pointer {
+			continue
+		}
+		pE := t.Elem()
+		if pE.Kind() != reflect.Struct {
+			continue
+		}
+		rV := reflect.ValueOf(p).Elem()
+		pnf := pE.NumField()
+		for i := 0; i < pnf; i++ {
+			sF := pE.Field(i)
+			pT, ok := sF.Tag.Lookup("env")
+			if !ok {
+				continue
+			}
+			if sF.Type.Kind() == reflect.String {
+				if eV, ok := g[pT]; ok {
+					rV.Field(i).SetString(eV)
+				}
+			} else if sF.Type.Kind() == reflect.Bool {
+				if eV, ok := g[pT]; ok {
+					rV.Field(i).SetBool(eV == "true")
+				}
+			}
 		}
 	}
 }
--- goenv_test.go
+++ goenv_test.go
@@ -1,16 +1,29 @@
 package goenv_test
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
 	goenv "github.com/Merapi-Tani-Instrument/GoEnv"
 )
 
+type EnvTest struct {
+	Coba  string `env:"spring.application.name"`
+	Coba1 bool   `env:"spring.main.web-environment"`
+}
+
 func TestLoadEnv(t *testing.T) {
 	f, err := os.Open("./sample.yaml")
 	if err != nil {
 		panic(err)
 	}
-	goenv.Parse(f)
+	gE, err := goenv.Parse(f)
+	if err != nil {
+		panic(err)
+	}
+
+	var gt EnvTest
+	gE.Inject(&gt)
+	fmt.Println("gt", gt)
 }
